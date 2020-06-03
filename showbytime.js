e221d33db972f6381b02c98d645f171c2695e5c1
ARC20 <58632172+ARC20@users.noreply.github.com>
2020-06-03 23:46:03 +0800

    Add files via upload

---
+++ showbytime.js
@@ -0,0 +1,10 @@
+let s = new Date('June 21, 2020 00:00:00').getTime(),
+    z = setInterval(function() {
+
+      let n = new Date().getTime();
+
+  // If the count down is over, write some text 
+        if (s < n) {
+        clearInterval(z); 
+        document.getElementById("showbytime").style.display=''; } 
+    });
---
+++ timeup.js
@@ -0,0 +1,10 @@
+let CD = new Date('June 22, 2020 00:00:00').getTime(),
+    y = setInterval(function() {
+
+      let N = new Date().getTime();
+
+  // If the count down is over, write some text 
+        if (CD < N) {
+        clearInterval(y); 
+        document.getElementById("showbytime").style.display='none'; } 
+    });
