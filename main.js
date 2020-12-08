580348abbd39cd543c869a7764255868b955d5f1
ARC20 <58632172+ARC20@users.noreply.github.com>
2020-12-08 17:36:19 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const selectElement = (element) => document.querySelector(element);
 
 selectElement('.mobile-menu').addEventListener('click', () => {
 	selectElement('header').classList.toggle('active');
+	selectElement('body').classList.toggle('lock');
 
 });
